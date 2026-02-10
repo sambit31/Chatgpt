9247d8da50a2e72e7e63d266f93bd829355284f3
Sambit Ghosh <sambitg31@gmail.com>
2026-02-10 14:36:32 +0530

    mongo updated

--- project_ChatGPT/backend/src/db/db.mongoose.js
+++ project_ChatGPT/backend/src/db/db.mongoose.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
